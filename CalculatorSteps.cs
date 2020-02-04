0c655d8d681b3eba979f0c9e60af580809f98332
Egor <arara1196@gmail.com>
2020-02-04 20:24:52 +0300

    whole package system test (successful Calculator test)

---
+++ CalculatorSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Example;
+using NUnit.Framework;
+
+namespace REST_test
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private int result;
+        Calculator calc = new Calculator();
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            calc.FirstNum = p0;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        {
+            calc.SecondNum = p0;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calc.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(p0, result);
+        }
+    }
+}
